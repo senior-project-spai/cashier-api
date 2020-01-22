79e1bc20952a3e864db4388e3d8208ea8c40e793
Supakorn Wongsawang <supakorn.wo@ku.th>
2020-01-22 17:21:51 +0700

    fix cors

--- app/main.py
+++ app/main.py
@@ -16,13 +16,7 @@ import os
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=['*'],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+app.add_middleware(CORSMiddleware, allow_origins=['*'])
 
 
 @app.on_event('startup')
